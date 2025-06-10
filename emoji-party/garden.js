118c94b238cea68d0895f94428fe914e7b080e39
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2025-06-10 18:43:09 +0200

    Update garden.js

--- emoji-party/garden.js
+++ emoji-party/garden.js
@@ -1,58 +1,58 @@
 let NUM_ROWS = 10;
 let NUM_COLS = 10;
 let NUM_VISITORS = 10;
-let NUM_SNACKS = 5;
+let NUM_TREASURES = 5;
 
-loadWorld();
+loadOcean();
 
 document.addEventListener('input', function(e) {
-  updateWorld();
-  doAGarden();
+  updateOcean();
+  doAnOcean();
 });
 
-function loadWorld() {
-  const json = localStorage.getItem('__emoji_garden__') || '{}';
+function loadOcean() {
+  const json = localStorage.getItem('__emoji_ocean__') || '{}';
   let state = JSON.parse(json);
   if (json == '{}') {
     // Save the defaults.
-    const state = {rows:NUM_ROWS, cols:NUM_COLS, visitors:NUM_VISITORS, snacks:NUM_SNACKS};
-    localStorage.setItem('__emoji_garden__', JSON.stringify(state));
+    const state = {rows:NUM_ROWS, cols:NUM_COLS, visitors:NUM_VISITORS, treasures:NUM_TREASURES};
+    localStorage.setItem('__emoji_ocean__', JSON.stringify(state));
   } else {
     document.getElementById('rows').value = NUM_ROWS = state.rows;
     document.getElementById('cols').value = NUM_COLS = state.cols;
     document.getElementById('visitors').value = NUM_VISITORS = state.visitors;
-    document.getElementById('snacks').value = NUM_SNACKS = state.snacks;
+    document.getElementById('treasures').value = NUM_TREASURES = state.treasures;
   }
 }
 
-function updateWorld() {
+function updateOcean() {
   NUM_ROWS = document.getElementById('rows').value;
   NUM_COLS = document.getElementById('cols').value;
   NUM_VISITORS = document.getElementById('visitors').value;
-  NUM_SNACKS = document.getElementById('snacks').value;
-  
-  const state = {rows:NUM_ROWS, cols:NUM_COLS, visitors:NUM_VISITORS, snacks:NUM_SNACKS};
-  localStorage.setItem('__emoji_garden__', JSON.stringify(state));
+  NUM_TREASURES = document.getElementById('treasures').value;
+
+  const state = {rows:NUM_ROWS, cols:NUM_COLS, visitors:NUM_VISITORS, treasures:NUM_TREASURES};
+  localStorage.setItem('__emoji_ocean__', JSON.stringify(state));
 }
 
-function doAGarden() {
+function doAnOcean() {
   const world = {
-      'v1': ['💁‍♀️', '🙋‍♂️', '💃', '🕺', '👯‍♀️', '👯‍♂️', '👬', '👭', '💑', '💏', '🐶'],
-      'v2': ['🎂','🍹','🍾', '🎁', '🌮', '🍕', '🎉', '🎉', '🎉', '🎉', '🎈', '🎊', ],
-      'm1': ['💚', '💚', '▫️', '▫️'],
-      'm2': ['🎈', '🎈', '▫️', '▫️'],
-      'm3': ['💙', '💙', '▫️', '▫️'],
-      'm4': ['💕', '💕', '▫️', '▫️'],
-      'm5': ['💜', '💜', '▫️', '▫️'],
-      'm6': ['🎈', '🎈', '▫️', '▫️'],
-      'm7': ['💖', '💖', '▫️', '▫️'],
+      'v1': ['🐙', '🐠', '🧜🏽‍♀️', '🐬', '🐳', '🧜‍♀️', '🧜‍♂️', '🦑', '🦈', '🧜', '🐋'],
+      'v2': ['🛟','🏝️','⚓️', '⚓️', '⚓️', '⚓️', '🛶', '🦀', '🛳', '🐡', '🛟', '💎'],
+      'm1': ['🌊', '🌊', '▫️', '▫️'],
+      'm2': ['🪸', '🪸', '▫️', '▫️'],
+      'm3': ['🪼', '🪼', '▫️', '▫️'],
+      'm4': ['🐚', '🐚', '▫️', '▫️'],
+      'm5': ['⛵️', '⛵️', '▫️', '▫️'],
+      'm6': ['🪨', '🪨', '▫️', '▫️'],
+      'm7': ['🫧', '🫧', '▫️', '▫️'],
     }
   const grammar = window.tracery.createGrammar(world);
-  
+
   // The initial pattern. 
-  // m : main
+  // m : main ocean tile
   // a : animal visitor
-  // l : special leaf
+  // l : treasure
   const pattern = fillArray('m'.repeat(NUM_COLS), NUM_ROWS);
 
   // Add random animal visitors.
@@ -61,16 +61,14 @@ function doAGarden() {
     const col = getRandomInt(NUM_COLS) - 1;
     pattern[row] = setCharAt(pattern[row], col, 'a');
   }
-  
-  // Add random snaxx.
-  for (let i = 0; i <= NUM_SNACKS; i++) {
+
+  // Add random treasures.
+  for (let i = 0; i <= NUM_TREASURES; i++) {
     const row = getRandomInt(NUM_ROWS) - 1;
     const col = getRandomInt(NUM_COLS) - 1;
     pattern[row] = setCharAt(pattern[row], col, 'l');
   }
-  
-  // console.log(pattern);
-  
+
   // Fill in the randoms with actual emoji
   const main = `#m${getRandomInt(7)}#`;
   for (let i = 0; i < pattern.length; i++) {
@@ -78,14 +76,12 @@ function doAGarden() {
     pattern[i] = pattern[i].replace(/a/g, '#v1#');
     pattern[i] = pattern[i].replace(/l/g, '#v2#');
   }
-  
-  //console.log(pattern);
+
   const box = pattern.join('\n');
   const story = grammar.flatten(box);
-  
+
   let output = document.getElementById('output');
   output.innerText = story;
-  //console.log(story);
 }
 
 function getRandomInt(max) {
